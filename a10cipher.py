9c8e2f52bd6dc393cab97d9d262f6e29fecd1552
damacaner <92731060+damacaner@users.noreply.github.com>
2022-01-14 14:10:33 +0200

    Update a10cipher.py

--- a10cipher.py
+++ a10cipher.py
@@ -3,6 +3,39 @@ from playsound import playsound
 from pydub import AudioSegment
 import numpy as np
 import sys
+import os
+import argparse
+from cryptography.fernet import Fernet
+from pathlib import Path
+def write_key():
+    #Generate a key and store it
+    key = Fernet.generate_key()
+    with open("encryption_key.txt", "wb") as file:
+        file.write(key)
+    return key
+def load_key():
+    return open("encryption_key.txt","rb").read()
+def get_original_extension_of_file(foldername,key):
+    files = Path(foldername).glob("*")
+    global original_extension
+    with open("extension.txt","w") as file2:
+        for file in files:
+            file_name, file_extension = os.path.splitext(file)
+            original_extension = file_extension
+        file2.write(original_extension)
+    return original_extension
+def load_extension():
+    return open("extension.txt","r").read()
+def change_extensions_in_folder(foldername,key):
+    files = Path(foldername).glob("*")
+    for file in files:
+        base = os.path.splitext(file)[0]
+        os.rename(file, base + ".AAAAA")
+def decrypt_in_folder(foldername,key,original_extension):
+    files = Path(foldername).glob("*")
+    for file in files:
+        base = os.path.splitext(file)[0]
+        os.rename(file, base + original_extension)
 def hill_encryption(plain, key):
     #Add a zero for every odd number
     global encrypted_text
@@ -131,11 +164,49 @@ def A10_decryption(cipher,key):
             decrypt_text += chr((temp2 % 26) + 65)
     print("Decrypted text> {}".format(decrypt_text))
     print("and it is time for BEHEADED KAMIKAZE!...")
-    playsound("Serious Sam Kamikaze scream.wav")
-
-
-
-plaintext = "fuck... "
+    # playsound("Serious Sam Kamikaze scream.wav")
+class FileOfType:
+    def __init__(self, type):
+        self.type = type
+        assert type in ['dir', 'file']
+    def __call__(self, path):
+        if self.type == 'dir':
+            if not os.path.isdir(path):
+                raise argparse.ArgumentTypeError(f"{path} is not a directory")
+        elif self.type == 'file':
+            if not os.path.isfile(path):
+                raise argparse.ArgumentTypeError(f"{path} is not a regular file")
+            elif os.path.islink(path):
+                raise argparse.ArgumentTypeError(f"{path} is a symbolic link")
+        return path
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Change every file extension to .AAAAA in folder")
+    parser.add_argument("-dir","--directory", type=FileOfType("dir"))
+    parser.add_argument("-ge","-getextension",action="store_true",help="Gets extension of files in folders to decrypt in the future")
+    parser.add_argument("-g", "--generate-key", dest="generate_key", action="store_true",
+                        help="Whether to generate a new key or use existing")
+    parser.add_argument("-e","--encrypt",action="store_true",help="Encrypts every file with .AAAAA extension key")
+    parser.add_argument("-d","--decrypt",action="store_true",help="Decrypts every file with .AAAAA extension key")
+    args = parser.parse_args()
+    print(args)
+generate_key = args.generate_key
+if generate_key:
+     write_key()
+key = load_key()
+getextension_ = args.ge
+if getextension_:
+    directory = args.directory
+    get_original_extension_of_file(directory, key)
+original_extension = load_extension()
+directory_ = args.directory
+encrypt_ = args.encrypt
+decrypt_ = args.decrypt
+directory = args.directory
+if (directory_ != None) and (encrypt_ == True):
+    change_extensions_in_folder(directory,key)
+elif (directory_ != None) and (decrypt_ == True):
+    decrypt_in_folder(directory, key, original_extension)
+plaintext = "fuck..."
 plaintext = plaintext.upper().replace(" ","")
 key = "hill"
 key = key.upper().replace(" ","")
