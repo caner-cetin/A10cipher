4b7a441519e5f4cf0229e8cd90717d6ce19582bf
damacaner <92731060+damacaner@users.noreply.github.com>
2022-04-23 20:35:33 +0300

    Update a10cipher.py

--- a10cipher.py
+++ a10cipher.py
@@ -4,7 +4,6 @@ import numpy as np
 import sys
 import os
 import argparse
-from collections import Counter
 from pathlib import Path
 import time
 
@@ -66,7 +65,7 @@ class Encryptor():
         upperdict = self.upperinverted
         s = ""
         deciphered = ""
-        y = file_data.split(" ")
+        y =msg.split(" ")
         print(y)
         for s in y:
             if "\n" in s:
@@ -83,8 +82,6 @@ class Encryptor():
                 deciphered += "|"
             s = ""
         print(deciphered)
-        with open(path, "w") as f:
-            f.write(deciphered)
         return deciphered
 
     def encrypt_file(self, file_name, path):
@@ -149,9 +146,9 @@ def get_original_extension_of_file(foldername: object, key: object) -> object:
     return original_extension
 
 
-def encrypt_txt_files():
+def encrypt_txt_files(dir):
     # Find subfolders
-    for folder in os.listdir(os.getcwd()):
+    for folder in os.listdir(dir):
         if os.path.isdir(folder):
             # Find files in subfolders
             for file in os.listdir(folder):
@@ -186,148 +183,6 @@ def change_extensions_in_folder(foldername, key):
     for file in files:
         base = os.path.splitext(file)[0]
         os.rename(file, base + ".AAAAA")
-
-
-def decrypt_in_folder(foldername, key, original_extension):
-    files = Path(foldername).glob("*")
-    for file in files:
-        base = os.path.splitext(file)[0]
-        os.rename(file, base + original_extension)
-
-
-def hill_encryption(plain, key):
-    # Add a zero for every odd number
-    global encrypted_text
-    global mule_inv
-    len_chk = 0
-    if ((len(plain) % 2) != 0):
-        plain += "0"
-        len_chk = 1
-    # Create Hill 2x2 matrixes
-    row = 2
-    col = int(len(plain) / 2)
-    msg2d = np.zeros((row, col), dtype=int)
-    itr1 = 0
-    itr2 = 0
-    for i in range((len(plain))):
-        if (i % 2 == 0):
-            msg2d[0][itr1] = int(ord(plain[i]) - 65)
-            itr1 += 1
-        else:
-            msg2d[0][itr2] = int(ord(plain[i]) - 65)
-            itr2 += 1
-    # Create a key
-    key2d = np.zeros((2, 2), dtype=int)
-    itr3 = 0
-    for i in range(2):
-        for j in range(2):
-            key2d[i][j] = ord(key[itr3]) - 65
-            itr3 += 1
-    print(key2d)
-    # Check validity of the key, first find determinant
-    deter = key2d[0][0] * key2d[0][1] * key2d[1][0] * key2d[1][1]
-    deter = deter % 26
-    mule_inv = 0
-    # Find multiplicative inverse
-    for i in range(26):
-        temp_inv = deter * i
-        if temp_inv % 26 == 1:
-            mule_inv = i
-            break
-        else:
-            continue
-
-    if mule_inv == -1:
-        print("Invalid key")
-        sys.exit()
-    encrypted_text = ""
-    itr_count = int(len(plain) / 2)
-    if len_chk == 0:
-        for i in range(itr_count):
-            temp1 = (msg2d[0][i] * key2d[0][0]) + (msg2d[1][i] * key2d[0][1])
-            encrypted_text += chr((temp1 % 26) + 65)
-            temp2 = (msg2d[0][i] * key2d[1][0]) + (msg2d[1][i] * key2d[1][1])
-            encrypted_text += chr((temp2 % 26) + 65)
-    else:
-        for i in range(itr_count - 1):  # Subtract 1 zero we added above
-            temp1 = (msg2d[0][i] * key2d[0][0]) + (msg2d[1][i] * key2d[0][1])
-            encrypted_text += chr((temp1 % 26) + 65)
-            temp2 = (msg2d[0][i] * key2d[1][0]) + (msg2d[1][i] * key2d[1][1])
-            encrypted_text += chr((temp2 % 26) + 65)
-    print("Encrypted text: {} ".format(encrypted_text))
-    return encrypted_text
-
-
-def A10_decryption(cipher, key):
-    global mule_inv
-    len_chk = 0
-    if (len(cipher) % 2) != 0:
-        cipher += "0"
-        len_chk = 1
-    row = 2
-    column = int(len(cipher) / 2)
-    msg2d = np.zeros((row, column), dtype=int)
-    itr1 = 0
-    itr2 = 0
-    for i in range(len(cipher)):
-        if i % 2 == 0:
-            msg2d[0][itr1] = int(ord(cipher[i]) - 65)
-            itr1 += 1
-        else:
-            msg2d[1][itr2] = int(ord(cipher[i]) - 65)
-            itr2 += 1
-    # Create a key
-    key2d = np.zeros((2, 2), dtype=int)
-    itr3 = 0
-    for i in range(2):
-        for j in range(2):
-            key2d[i][j] = ord(key[itr3]) - 65
-            itr3 += 1
-    # Find determinant
-    deter = key2d[0][0] * key2d[1][1] - key2d[0][1] * key2d[1][0]
-    deter = deter % 26
-    # Find multiplicative inverse
-    for i in range(26):
-        temp_inv = deter * i
-        if (temp_inv % 26) == 1:
-            mule_inv = i
-            break
-        else:
-            continue
-    # Adjugate matrix
-    # swapping
-    key2d[0][0], key2d[1][1] = key2d[1][1], key2d[0][0]
-    # change signs
-    key2d[0][1] *= -1
-    key2d[1][0] *= -1
-    key2d[0][1] = key2d[0][1] % 26
-    key2d[1][0] = key2d[1][0] % 26
-    mule_inv = 0
-    for i in range(2):
-        for j in range(2):
-            key2d[i][j] *= mule_inv
-    for i in range(2):
-        for j in range(2):
-            key2d[i][j] = key2d[i][j] % 26
-    decrypt_text = ""
-    iteration_count = int(len(cipher) / 2)
-    if len_chk == 0:
-        for i in range(iteration_count):
-            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
-            decrypt_text += chr((temp1 % 26) + 65)
-            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
-            decrypt_text += chr((temp2 % 26) + 65)
-    else:
-        for i in range(iteration_count - 1):
-            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
-            decrypt_text += chr((temp1 % 26) + 65)
-            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
-            decrypt_text += chr((temp2 % 26) + 65)
-    print("Decrypted text> {}".format(decrypt_text))
-    print("and it is time for BEHEADED KAMIKAZE!...")
-    # playsound("Serious Sam Kamikaze scream.wav")
-
-
 class FileOfType:
     def __init__(self, type):
         self.type = type
@@ -343,8 +198,6 @@ class FileOfType:
             elif os.path.islink(path):
                 raise argparse.ArgumentTypeError(f"{path} is a symbolic link")
         return path
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Change every file extension to .AAAAA in folder")
     parser.add_argument("-dir", "--directory", type=FileOfType("dir"))
@@ -352,12 +205,9 @@ if __name__ == "__main__":
                         help="Gets extension of files in folders to decrypt in the future")
     parser.add_argument("-g", "--generate-key", dest="generate_key", action="store_true",
                         help="Whether to generate a new key or use existing")
-    parser.add_argument("-e", "--encrypt", action="store_true", help="Encrypts every file with .AAAAA extension key")
-    parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypts every file with .AAAAA extension key")
+    parser.add_argument("-e", "--encrypt", action="store_true", help="Encrypts every txt file with AAAA alphabet")
+    parser.add_argument("-d", "--decrypt", action="store_true", help="Decrypts every txt file with AAAA alphabet")
     parser.add_argument("-f", "--file", type=FileOfType("file"))
-    parser.add_argument("-et", "--encrypttext", action="store_true", help="Encrypts a .txt file with .AAAAA alphabet.")
-    parser.add_argument("-nsdir", "--notsuredir", type=FileOfType("dir"),
-                        help="Use this dir option if you have many subfolders in that directory")
     args = parser.parse_args()
 if len(sys.argv) < 2:
     encrypt_txt_files()
@@ -370,14 +220,25 @@ decrypt_ = args.decrypt
 directory = args.directory
 file = args.file
 encrypttext_ = args.encrypttext
-dir2 = args.notsuredir
 
 if encrypttext_ == True:
     print("Please enter a string to encrypt, leave this blank if you want to encrypt a .txt file")
     message = input()
 if (directory_ != None) and encrypttext_ == True:
-    pass
-if (dir2 != None) and encrypttext_ == True:
-    for root, dirs, files in os.walk(directory):
-        for dirname in dirs:
-            pass
+    encrypt_txt_files(directory_)
+if (directory_ != None) and decrypttext_ == True:
+    decrypt_txt_files(directory_)
+if (file != None) and encrypttext_ == True:
+    encryptor = Encryptor()
+    with open(file, "r") as f:
+        file_data = f.read()
+    # Get path of file
+    path = os.path.join(file)
+    encryptor.encrypt_file(file_data, path)
+if (file != None) and decrypttext_ == True:
+    encryptor = Encryptor()
+    with open(file, "r") as f:
+        file_data = f.read()
+    # Get path of file
+    path = os.path.join(file)
+    encryptor.decrypt_file(file_data, path)
